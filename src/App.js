f94ce408ad9b9ec46d3011417c8939413231c5aa
pablochile2017 <pablochile2017@gmail.com>
2019-06-05 12:13:44 +0000

    agregado el quitar datos de la base al presionar X

--- src/App.js
+++ src/App.js
@@ -61,11 +61,37 @@ class App extends Component {
 
 
         borrarTexto = (index) => {
-            this.setState({
+           /* this.setState({
               base: this.state.base.filter((e,i) => {
                 return i !== index
               })
-            })
+            })*/
+
+                fetch('https://assets.breatheco.de/apis/fake/todos/user/juan', {
+                method: "PUT",
+                body: JSON.stringify([...this.state.base.filter((e,i) => {
+                return i !== index
+                })
+              ]),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+                })
+                .then(resp => {
+                    return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+                })
+                .then(data => {
+
+                    //here is were your code should start after the fetch finishes
+                    console.log(data); //this will print on the console the exact object received from the server
+                    this.obtenerDatos();
+                })
+                .catch(error => {
+                    //error handling
+                    console.log(error);
+                });
+
+
         }
 
 
